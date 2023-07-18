d398fb96e90eb2143af6ddb18abaeda82afa9abf
Noah <noaligpitan26@gmail.com>
2023-07-18 21:03:24 +0800

    configure next confige.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { images: { unoptimized: true } };
+const nextConfig = {
+  images: { unoptimized: true },
+  experimental: {
+    "@mui/material": {
+      transform: "@mui/material/{{member}}",
+    },
+    "@mui/icons-material": {
+      transform: "@mui/icons-material/{{member}}",
+    },
+  },
+};
 
 module.exports = nextConfig;
