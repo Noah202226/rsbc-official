11c7597df0056f8694daf6c06c3fcc34c40e6ed7
Noah <noaligpitan26@gmail.com>
2023-07-19 13:26:36 +0800

    add useEffect in notfoundjs

--- app/not-found.js
+++ app/not-found.js
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function NotFound() {
   const router = useRouter();
